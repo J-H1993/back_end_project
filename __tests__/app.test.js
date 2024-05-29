8c597fabd314f06d9b86ca60a85c4f4c3bef1bc8
john <jkhowarth93@gmail.com>
2024-05-29 10:29:53 +0100

    Completed task 4, added in a get article by ID method with completed error handling. Added in a generic 404 catcher and a custom 404 for an invalid article id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,6 @@ const app = require('../app')
 const request = require('supertest')
 const data = require('../db/data/test-data/index')
 const seed = require('../db/seeds/seed')
-const fs = require('fs/promises')
 const endpoints = require('../endpoints.json')
 
 beforeEach(() => seed(data))
@@ -34,7 +33,41 @@ describe('GET /api', ()=>{
         .expect(200)
         .then(({body})=>{
         expect(body.endPoints).toEqual(endpoints)
-           
         })
     });
 })
+
+describe('GET /api/articles/4', ()=>{
+    test('should return a status 200 if the data is found and return a single article of the correct id',() =>{
+        return request(app)
+        .get('/api/articles/4')
+        .expect(200)
+        .then(({body})=>{
+        expect(body.article.article_id).toBe(4)
+        })
+    })      
+    });
+
+describe('GET /api/articles/9999999', () => {
+    test('should return a status code of 404 and a message of article not found', () => {
+        return request(app)
+        .get('/api/articles/999999')
+        .expect(404)
+        .then(({body})=>{
+            expect(body.msg).toBe("Article not found")
+        })
+        
+    });
+})
+
+
+describe('.all(*) - handles any endpoint that does not exist', () =>{
+    test('should return a 404 error code and the message that the route has not been found', () => {
+        return request(app)
+        .get("/api/none-existent-endpoint")
+        .expect(404)
+        .then(({body})=>{
+          expect(body.msg).toBe("Route not found")
+            })
+        });
+    })
