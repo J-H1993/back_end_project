e495b957b701ff05055a3b022afa3bb883e28757
john <jkhowarth93@gmail.com>
2024-06-11 18:53:55 +0100

    "fixed a typo in the scripts file that may have been causing an error"

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const ENV = process.env.NODE_ENV || 'development';
 const config={}; 
 if(ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL
-  config.max =2;
+  config.max = 2;
 }
 
 require('dotenv').config({
